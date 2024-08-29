22afc6d516d95ef57d77ee74570fa8468cf3012c
Emile Decosterd <edautomation20@gmail.com>
2024-08-29 15:15:59 +0200

    Implement writer in C

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,8 +1,43 @@
 #include <syslog.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <string.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[])
 {
+
     openlog("writersyslog", 0, LOG_USER);
-    syslog(LOG_INFO, "Hello, syslog!");
+
+    int nargs = argc - 1; // Ignore name of the program
+    if (nargs != 2)
+    {
+        syslog(LOG_ERR, "Expected 2 arguments, got %i\n", nargs);
+        printf("Expected 2 arguments, got %i\n", nargs);
+        return 1;
+    }
+    
+    char* filename = argv[1];
+    int fd = creat(filename, 0644);
+    if (fd == -1)
+    {
+        int error_nbr = errno;
+        perror("Error creating file: ");
+        syslog(LOG_ERR, "Could not create file.");
+        printf("Error creating file. Errno = %i\n", error_nbr);
+        return 1;
+    }
+    else
+    {
+        printf("Successfully created file: %s\n", filename);
+    }
+
+
+    char* text = argv[2];
+    size_t len = strlen(text);
+    write(fd, text, len);
+    close(fd);
     return 0;
 }
